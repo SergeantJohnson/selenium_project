25e572b31d19a7d4eb280268e26f6f130eaf6f7d
SergeantJohnson <charulata.pattekar@gmail.com>
2022-01-17 16:40:45 +0530

    trial1

--- basicweb/chkbox.py
+++ basicweb/chkbox.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
+#this is a comment
 
 driver = webdriver.Chrome(service=Service("C:\\Users\\charu\\workspace_python\\drivers\\chromedriver"))
 driver.get("https://www.bing.com/account")
